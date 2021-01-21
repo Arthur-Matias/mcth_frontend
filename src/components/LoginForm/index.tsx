0233f995ce6d8b6fc3f4eea54af89a8167601cd2
Arthur Matias <ahmmfdc@gmail.com>
2021-01-21 18:40:14 -0300

    Login form component

---
+++ src/components/LoginForm/index.tsx
@@ -0,0 +1,59 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faFacebookF, faGithubAlt, faGoogle} from '@fortawesome/free-brands-svg-icons';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PrimaryButton from '../PrimaryButton';
+import './styles.scss';
+
+interface ButtonInterface {
+    isOrange?: boolean;
+    text?: string
+}
+
+const LoginForm: React.FC<ButtonInterface> = ({
+    text,
+    isOrange
+}) => {
+    return (
+        <div className="login-form-container">
+            <div className="login-wrapper">
+                <h3>
+                    <b>Entre com</b>
+                </h3>
+                <div className="social-login">
+                    <div className="social-icon-container">
+                        <FontAwesomeIcon icon={faGoogle} />
+                    </div>
+                    <div className="social-icon-container">
+                        <FontAwesomeIcon icon={faFacebookF} />
+                    </div>
+                    <div className="social-icon-container">
+                        <FontAwesomeIcon icon={faGithubAlt} />
+                    </div>
+                </div>
+                <p className='orText'>ou</p>
+                <form className='login-form'>
+                    <div className="common-login">
+                    <div className="login-input-wrapper">
+                        <span className="input-icon-container" ></span>
+                        <input type="email" id="userMail" placeholder="E-mail" />
+                    </div>
+                            <div className="login-input-wrapper">
+                                <span className="input-icon-container"></span>
+                                <input type="password" id="userPass" placeholder="Senha" />
+                            </div>
+                    </div>
+                    <div className="form-button">
+                        <PrimaryButton text='Entrar' ></PrimaryButton>
+                    </div>
+
+                    <p className='orText'>ou</p>
+                    <Link to='/register'>Registre-se</Link>
+                </form>
+            </div>
+        </div>
+    )
+};
+
+export default LoginForm;
+
