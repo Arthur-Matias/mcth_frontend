4c44fce3b25081ce3ef6ab0e672b2dc6518b92c2
Arthur Matias <ahmmfdc@gmail.com>
2021-01-21 19:27:35 -0300

    finishing Login page

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -1,19 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faFacebookF, faGithubAlt, faGoogle} from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope, faKey} from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PrimaryButton from '../PrimaryButton';
 import './styles.scss';
 
-interface ButtonInterface {
-    isOrange?: boolean;
-    text?: string
-}
-
-const LoginForm: React.FC<ButtonInterface> = ({
-    text,
-    isOrange
-}) => {
+const LoginForm = () => {
     return (
         <div className="login-form-container">
             <div className="login-wrapper">
@@ -29,26 +22,27 @@ const LoginForm: React.FC<ButtonInterface> = ({
                     </div>
                     <div className="social-icon-container">
                         <FontAwesomeIcon icon={faGithubAlt} />
+                        
                     </div>
                 </div>
                 <p className='orText'>ou</p>
                 <form className='login-form'>
                     <div className="common-login">
                     <div className="login-input-wrapper">
-                        <span className="input-icon-container" ></span>
+                        <span className="input-icon-container" ><FontAwesomeIcon icon={faEnvelope} /></span>
                         <input type="email" id="userMail" placeholder="E-mail" />
                     </div>
-                            <div className="login-input-wrapper">
-                                <span className="input-icon-container"></span>
-                                <input type="password" id="userPass" placeholder="Senha" />
-                            </div>
+                    <div className="login-input-wrapper">
+                        <span className="input-icon-container"><FontAwesomeIcon icon={faKey} /></span>
+                        <input type="password" id="userPass" placeholder="Senha" />
+                    </div>
                     </div>
                     <div className="form-button">
                         <PrimaryButton text='Entrar' ></PrimaryButton>
                     </div>
 
-                    <p className='orText'>ou</p>
-                    <Link to='/register'>Registre-se</Link>
+                    <p style={{marginBottom:'0.5rem'}} >ou</p>
+                    <Link to='/registro'>Registre-se</Link>
                 </form>
             </div>
         </div>
